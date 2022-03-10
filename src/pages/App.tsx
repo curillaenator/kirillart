644891a348619ee2698135bfee1c88d59b74c564
Arturov Kirill <kaarturov@sberbank.ru>
2022-03-10 18:08:27 +0300

    feat(app): firebase added

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,16 @@
 import React, { FC } from "react";
 import { Routes, Route } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import { Layout } from "./layout";
 import { Main } from "./main";
 import { Examples } from "./examples";
+
+import { FB_CONFIG } from "@src/api/constants";
+
+const app = initializeApp(FB_CONFIG);
+getAnalytics(app);
 
 export const App: FC = () => {
   return (
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
 // const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
@@ -38,6 +39,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "styles.css",
     }),
+    new Dotenv(),
 
     // new CopyWebpackPlugin({
     //   patterns: [
